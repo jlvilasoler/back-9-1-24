3c1b0a68a37a6f09be9cf55b689342bca2d570e9
jlvilasoler <jlvilasoler@hotmail.com>
2023-08-29 20:18:32 -0300

    prueba

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 const router = Router();
 
-import CartManager from "../src/cartManager.js";
-const cart = new CartManager("../src/cart.json");
 
 import ProductManager from "../src/productManager.js";
 const products = new ProductManager("../src/productManager.json");
+import CartManager from "../src/cartManager.js";
+const cart = new CartManager("../src/cart.json");
+
 
 
 const newCart = {id: 0 , products: []}
@@ -17,23 +18,45 @@ router.get('/cart', async (req, res) => {
     res.send(readCart);
 });
 
-
-router.get('/cart/:cid', async (req, res) => {
-    try {
+//GET 1
+router.post('/cart/:cid', async (req, res) => {
         const cid = parseInt(req.params.cid, 10);
         const cartId = await cart.getCartById(cid);
-
         if (!cartId) {
             res.status(404).send("Cart not found");
         } else {
             res.send(cartId.products);
+        }});
+        /*
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});*/
+
+
+
+
+
+
+
+
+//3
+router.post('/cart/:cid/product/:pid', async (req, res) => {
+    try {
+        const cartId = parseInt(req.params.cartId);
+        const { product, quantity } = req.body;
+
+        // Asegúrate de que `product` sea un objeto que contenga al menos `productId`, `name` y `price`
+        if (!product || !productcartId || !product.name || !product.price) {
+            throw new Error("El objeto de producto es inválido.");
         }
+
+        await cart.addProductCart(cartId, product, quantity);
+        res.json({ message: "Producto agregado al carrito correctamente" });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
-
-
 
 router.post('/cart/', async (req, res) => {
     await cart.addProductsToCart(newCart)
@@ -43,7 +66,7 @@ router.post('/cart/', async (req, res) => {
 
 
 // En la ruta GET, devuelve el array de productos que hay en el carrito - NO ANDA
-router.get('/cart/:cid', async (req, res) => {
+router.get('/:cid', async (req, res) => {
     try {
         const cid = parseInt(req.params.cid, 10);
         const cartItems = await cart.getCarts(); // Obtener todos los productos del carrito
@@ -80,7 +103,6 @@ router.post('/cart/:cartId', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
-
 
 export default router;
 
