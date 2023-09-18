26f78bd30354bbc102cd62074820877e8aef2e2b
jlvilasoler <jlvilasoler@hotmail.com>
2023-09-17 21:51:01 -0300

    se arreglo errores

--- routes/imgRouter.js
+++ routes/imgRouter.js
@@ -1,12 +1,13 @@
-import { Router } from 'express';
-import { uploader } from '../src/middlewares/multer.js';
+import { Router } from "express";
+import { uploader } from "../src/middlewares/multer.js";
 
 const router = Router();
 
-router.post('/', uploader.single('file'), async (req, res) => {
-    const img = req.body;
-    console.log(req.file)
+
+router.post('/', uploader.single('file'), (req, res) => {
+    const image  =req.body;
+    console.log(req.file);
     res.status(200).send();
 });
 
-export default router;
+export default router;  
